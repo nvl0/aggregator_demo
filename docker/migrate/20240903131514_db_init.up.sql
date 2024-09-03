eae310d3341604910ec17a363d7496bc5cd999ee
nick <nvl0>
2024-09-03 18:20:04 +0500

    build(docker, migrate): исправил докер, добавил миграции на будущее

--- docker/migrate/20240903131514_db_init.up.sql
+++ docker/migrate/20240903131514_db_init.up.sql
@@ -1,24 +1,28 @@
 CREATE TABLE
-    session
+    online_session
     (
         ip CHARACTER VARYING NOT NULL,
         sess_id INTEGER NOT NULL,
         nas_ip CHARACTER VARYING NOT NULL,
+        CONSTRAINT sess_id_ix1 UNIQUE (sess_id),
         PRIMARY KEY (ip)
     );
 
-INSERT INTO session (ip, sess_id, nas_ip) VALUES ('127.0.0.1', 2, '127.0.0.0');
+INSERT INTO online_session (ip, sess_id, nas_ip) VALUES ('127.0.0.1', 1, '127.0.0.0');
+INSERT INTO online_session (ip, sess_id, nas_ip) VALUES ('192.168.0.1', 2, '192.168.0.0');
 
 CREATE TABLE
     channel
     (
-        channel_id INTEGER NOT NULL,
+        channel_id SERIAL NOT NULL,
+        count_enabled BOOLEAN DEFAULT false NOT NULL,
         descr CHARACTER VARYING NOT NULL,
+        CONSTRAINT descr_ix1 UNIQUE (descr),
         PRIMARY KEY (channel_id)
     );
 
-INSERT INTO channel (channel_id, descr) VALUES (0, 'internal');
-INSERT INTO channel (channel_id, descr) VALUES (1, 'external');
+INSERT INTO channel (descr) VALUES ('internal');
+INSERT INTO channel (count_enabled, descr) VALUES (true, 'external');
 
 CREATE TABLE
     chunk
