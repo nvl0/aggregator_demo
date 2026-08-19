ae5fc0802e839d12c0bee8d534b7ad8eb0d7fbc2
nvl0 <nickjessop19@gmail.com>
2026-08-19 14:22:18 +0500

    feat(config): добавил размер пула воркеров worker_pool.size

--- src/config/config.go
+++ src/config/config.go
@@ -3,9 +3,13 @@ package config
 import (
 	"fmt"
 	"os"
+	"strconv"
 
 	"github.com/jinzhu/configor"
 )
+
+// defaultWorkerPoolSize размер пула воркеров агрегации по умолчанию
+const defaultWorkerPoolSize = 10
 
 // Config конфиг
 type Config struct {
@@ -14,6 +18,9 @@ type Config struct {
 		Password string `yaml:"password"`
 		URL      string `yaml:"url"`
 	} `yaml:"postgres"`
+	WorkerPool struct {
+		Size int `yaml:"size"`
+	} `yaml:"worker_pool"`
 }
 
 // NewConfig инициализация конфига проекта
@@ -31,4 +38,20 @@ func (c *Config) PostgresURL() string {
 	}
 	return fmt.Sprintf("postgresql://%s:%s@%s/aggregator?sslmode=disable&timezone=Asia/Tashkent",
 		c.Postgres.User, c.Postgres.Password, c.Postgres.URL)
+}
+
+// WorkerPoolSize размер пула воркеров агрегации.
+// Значение из conf.yaml переопределяется переменной окружения WORKER_POOL_SIZE.
+func (c *Config) WorkerPoolSize() int {
+	if envSize := os.Getenv("WORKER_POOL_SIZE"); envSize != "" {
+		if size, err := strconv.Atoi(envSize); err == nil && size > 0 {
+			c.WorkerPool.Size = size
+		}
+	}
+
+	if c.WorkerPool.Size < 1 {
+		c.WorkerPool.Size = defaultWorkerPoolSize
+	}
+
+	return c.WorkerPool.Size
 }
